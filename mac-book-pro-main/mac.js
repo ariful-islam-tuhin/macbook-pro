ad801a1f1a651617cf5a545741e346dd613c0533
ariful.islam.tuhin.github <ariful.islam.tuhin.1830@gmail.com>
2021-08-21 22:50:05 +0600

    complet all

--- mac-book-pro-main/mac.js
+++ mac-book-pro-main/mac.js
@@ -1,31 +1,29 @@
+// catch first two button
 const memory8 = document.getElementById("memory8gb");
 const memory16 = document.getElementById("memory16gb");
-//
+// catch second three button
 const storage256 = document.getElementById("storage256");
 const storage512 = document.getElementById("storage512");
 const storage1T = document.getElementById("storage1T");
-//
+// catche third two button
 const freedelivary = document.getElementById("freedelivary");
 const paiddelivary = document.getElementById("paiddelivary");
-//
-
+// catch whice will be change coast and fixed price
 const fixedprice = document.getElementById("fixedprice");
 const memorycoast = document.getElementById("memorycoast");
 const storegecoast = document.getElementById("storegecoast");
 const deliverycoast = document.getElementById("deliverycoast");
-//
-
+// catch total
 const total = document.getElementById("total");
-//
+// catch last three apply promocode and final discount price
 const promocode = document.getElementById("promocode");
 const apply = document.getElementById("apply");
 const finalTeaxt = document.getElementById("final");
-// update all
+// update all by function
 function updateAll() {
   var a = Number(memorycoast.innerText);
   var b = Number(storegecoast.innerText);
   var c = Number(deliverycoast.innerText);
-
   // var abc = a+b+c;
   var d = Number(fixedprice.innerText);
   var abcd = a + b + c + d;
